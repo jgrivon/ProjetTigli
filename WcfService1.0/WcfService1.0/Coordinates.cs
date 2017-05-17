53f87e3cbbbdb14a8d0c50fd2442919d0ff8915b
Zineb EL HAOUARI <zineb.el-haouari@etu.unice.fr>
2017-05-17 14:11:33 +0200

    Mock Service WCF
    to be integrated

---
+++ WcfService1.0/WcfService1.0/Coordinates.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Linq;
+using System.Web;
+
+
+namespace WcfService1._0
+{
+  [DataContract]
+  public class Coordinates
+  {
+    [DataMember]
+    public long Longitude
+    {
+      get { return Longitude;   }
+      set { Longitude = value;  }
+    }
+
+    [DataMember]
+    public long Lattitude
+    {
+      get { return Lattitude;   }
+      set { Lattitude = value;  }
+    }
+  }
+}
---
+++ WcfService1.0/WcfService1.0/Service1.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService1._0
+{
+  // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" à la fois dans le code, le fichier svc et le fichier de configuration.
+  // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+  public class Service1 : IService1
+  {
+    public long CalculTrajectory(Coordinates departure, Coordinates destination)
+    {
+      long result =
+      destination.Lattitude - departure.Lattitude
+        + destination.Longitude - departure.Longitude;
+
+      return 0;
+    }
+  }
+}
